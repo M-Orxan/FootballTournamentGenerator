e66b0c6a3c3bf9b12920fea5cd187030df009c53
Orxan Mustafayev <orxanm385@gmail.com>
2024-05-18 11:48:51 +0200

    ConnectionWithSql

---
+++ FootballTournamentGenerator/FootballTournamentGenerator/DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FootballTournamentGenerator.DAL
+{
+    public class AppDbContext: DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+
+
+    }
+}
---
+++ FootballTournamentGenerator/FootballTournamentGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballTournamentGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
