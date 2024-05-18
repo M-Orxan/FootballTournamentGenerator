76ff7e2bc893b751b65ccb83d7693a0f1d9f764c
Orxan Mustafayev <orxanm385@gmail.com>
2024-05-18 12:37:50 +0200

    ConvertingTemplateToMVC

---
+++ FootballTournamentGenerator/FootballTournamentGenerator/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using FootballTournamentGenerator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FootballTournamentGenerator.Controllers
+{
+    public class DashboardController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
